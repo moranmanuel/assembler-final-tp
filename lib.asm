58effce896e52f1594c97c4d399f5f55be6e8357
Mc.Patto <patriciomcallister4ae@gmail.com>
2025-11-13 09:49:52 -0300

    initial commit

---
+++ lib.asm
@@ -0,0 +1,678 @@
+.model small
+.stack
+
+.data
+tempTarget          db 5 dup (0)
+rw_base_column      db 0
+rw_base_row         db 0
+rw_letter_attr      db 0
+render_base_column  db 0
+big_letter_attr     db 0        ; Atributo temporal para letras grandes
+box_char_top_l      db 218      ; ┌ (esquina superior izquierda redondeada)
+box_char_top_r      db 191      ; ┐ (esquina superior derecha redondeada)
+box_char_bot_l      db 192      ; └ (esquina inferior izquierda redondeada)
+box_char_bot_r      db 217      ; ┘ (esquina inferior derecha redondeada)
+box_char_horiz      db 196      ; ─ (línea horizontal simple)
+box_char_vert       db 179      ; │ (línea vertical simple)
+
+; Arte ASCII para letras grandes estilo contorno (5x11 cada una, 55 bytes)
+; Usando _, -, | para crear solo los bordes/contornos
+; W - 11 caracteres de ancho     
+;     __    __
+;    / /   / /
+;   / /   / / 
+;  / // \/ /  
+; /__/ \__/   
+bigW db 177,177,' ',' ',' ',' ',' ',177,177,' ',' ',177,177,177,177,' ',' ',177,177,177,177,177,177,' ',' ',177,177,177,177,' ',' ',' ',' ',177,177,' ',' ',' ',177,177,' ',' ',' ',' ',177,177,' ',' ',177,177,0dh,0ah 
+     db 177,177,' ',' ',' ',' ',' ',177,177,' ',177,177,' ',' ',177,177,' ',177,177,' ',' ',' ',177,177,' ',177,177,' ',' ',177,177,' ',' ',177,177,' ',' ',' ',' ',177,177,' ',' ',177,177,' ',' ',' ',177,177,0dh,0ah 
+     db 177,177,' ',' ',177,' ',' ',177,177,' ',177,177,' ',' ',177,177,' ',177,177,177,177,177,177,' ',' ',177,177,' ',' ',' ',177,177,' ',177,177,' ',' ',' ',' ',' ',177,177,177,177,' ',' ',' ',' ',177,177,0dh,0ah 
+     db 177,177,' ',177,' ',177,' ',177,177,' ',177,177,' ',' ',177,177,' ',177,177,' ',' ',' ',177,177,' ',177,177,' ',' ',177,177,' ',' ',177,177,' ',' ',' ',' ',' ',' ',177,177,' ',' ',' ',' ',' ',' ',' ',0dh,0ah 
+     db ' ',177,177,' ',' ',' ',177,177,' ',' ',' ',177,177,177,177,' ',' ',177,177,' ',' ',' ',177,177,' ',177,177,177,177,' ',' ',' ',' ',177,177,177,177,177,' ',' ',' ',177,177,' ',' ',' ',' ',' ',177,177,0dh,0ah 
+
+; bigW db 177,177,32,32,32,32,32,32,32,177,177
+;      db 177,177,32,32,32,32,32,32,32,177,177
+;      db 177,177,32,32,177,32,32,32,32,177,177
+;      db 177,177,32,177,32,177,32,32,32,177,177
+;      db 32,177,177,32,32,32,177,177,32
+; O - 11 caracteres de ancho  
+bigO db 95,32,32,32,32,32,32,32,32,32,95
+     db 124,32,32,32,32,32,32,32,32,32,124
+     db 124,32,32,32,61,61,61,32,32,32,124
+     db 124,32,32,32,32,32,32,32,32,32,124
+     db 45,32,32,32,32,32,32,32,32,32,45
+; R - 11 caracteres de ancho
+bigR db 95,32,32,32,32,32,32,32,32,32,95
+     db 124,32,32,32,32,32,32,32,32,32,124
+     db 124,32,32,32,61,61,61,32,32,32,124
+     db 124,32,32,32,32,92,32,32,32,32,32
+     db 45,32,32,32,32,32,92,45,32,32,32
+; D - 11 caracteres de ancho
+bigD db 95,32,32,32,32,32,32,32,32,32,95
+     db 124,32,32,32,32,32,32,32,32,32,124
+     db 124,32,32,32,32,32,32,32,32,32,124
+     db 124,32,32,32,32,32,32,32,32,32,124
+     db 45,32,32,32,32,32,32,32,32,32,45
+; L - 11 caracteres de ancho
+bigL db 124,32,32,32,32,32,32,32,32,32,32
+     db 124,32,32,32,32,32,32,32,32,32,32
+     db 124,32,32,32,32,32,32,32,32,32,32
+     db 124,32,32,32,32,32,32,32,32,32,32
+     db 45,45,45,45,45,45,45,45,45,45,45
+; Y - 11 caracteres de ancho
+bigY db 92,32,32,32,32,32,32,32,32,32,47
+     db 32,92,32,32,32,32,32,32,32,47,32
+     db 32,32,124,32,32,32,32,124,32,32,32
+     db 32,32,124,32,32,32,32,124,32,32,32
+     db 32,32,124,32,32,32,32,124,32,32,32
+; ! - 11 caracteres de ancho
+bigAdm db 32,32,32,32,124,32,32,32,32,32,32
+      db 32,32,32,32,124,32,32,32,32,32,32
+      db 32,32,32,32,124,32,32,32,32,32,32
+      db 32,32,32,32,32,32,32,32,32,32,32
+      db 32,32,32,32,124,32,32,32,32,32,32
+
+.code
+
+GREEN_ATTR  EQU 2Fh
+YELLOW_ATTR EQU 6Fh
+BASE_ATTR   EQU 0Fh
+
+public SetVideoModeText
+public ClearScreenAttr
+public PrintDollarStringAt
+public PrintCenteredDollarString
+public DrawGuessSlots
+public ReadWord
+public EvaluateGuess
+public RenderGuessRow
+public CalculateCenteredColumn
+public DrawBigText
+
+SetVideoModeText proc near
+    mov ax, 0003h
+    int 10h
+    ret
+SetVideoModeText endp
+
+ClearScreenAttr proc near
+    push bx
+    push cx
+    push dx
+
+    mov bh, al
+    mov ax, 0600h
+    xor cx, cx
+    mov dx, 184Fh
+    int 10h
+
+    pop dx
+    pop cx
+    pop bx
+    ret
+ClearScreenAttr endp
+
+WriteCharAttr proc near
+    push bx
+    push cx
+    push dx
+    push di
+
+    mov dx, ax
+    mov al, bh
+    xor ah, ah
+    mov cl, 50h          ; 80 decimal
+    mul cl
+    mov di, ax
+    mov al, bl
+    xor ah, ah
+    add di, ax
+    shl di, 1
+    mov ax, 0B800h
+    mov es, ax
+    mov ax, dx
+    mov es:[di], ax
+
+    pop di
+    pop dx
+    pop cx
+    pop bx
+    ret
+WriteCharAttr endp
+
+PrintDollarStringAt proc near
+    push ax
+    push si
+
+NextChar:
+    lodsb
+    cmp al, '$'
+    je EndPrint
+    call WriteCharAttr
+    inc bl
+    jmp NextChar
+
+EndPrint:
+    pop si
+    pop ax
+    ret
+PrintDollarStringAt endp
+
+PrintCenteredDollarString proc near
+    ; Entrada: SI = puntero a string, BH = fila, AH = atributo
+    push ax
+    push bx
+    push cx
+    push dx
+    push si
+
+    mov dh, ah          ; Guardar AH (atributo) en DH
+    mov dl, bh          ; Guardar BH (fila) en DL
+    mov bx, si          ; BX = puntero al string para contar
+    xor cx, cx
+
+CountLoop:
+    lodsb
+    cmp al, '$'
+    je LengthReady
+    inc cx
+    jmp CountLoop
+
+LengthReady:
+    mov si, bx          ; Restaurar puntero al string
+    mov ax, 50h
+    sub ax, cx
+    shr ax, 1
+    mov bl, al          ; BL = columna centrada
+    mov bh, dl          ; Restaurar BH (fila) desde DL
+    mov ah, dh          ; Restaurar AH (atributo) desde DH
+    call PrintDollarStringAt
+
+    pop si
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    ret
+PrintCenteredDollarString endp
+
+DrawBox proc near
+    ; Dibuja un recuadro con líneas dobles
+    ; Entrada: BH = fila, BL = columna, AL = letra (o '_' si vacío), AH = atributo
+    push ax
+    push bx
+    push cx
+    push dx
+
+    mov dl, al          ; Guardar letra
+    mov dh, ah          ; Guardar atributo
+    mov cl, bl          ; Guardar columna base
+
+    ; Línea superior: ╔═══╗ (5 caracteres: ╔ + 3 ═ + ╗)
+    mov al, [box_char_top_l]
+    mov ah, dh
+    call WriteCharAttr
+    inc bl
+    mov al, [box_char_horiz]
+    call WriteCharAttr
+    inc bl
+    mov al, [box_char_horiz]
+    call WriteCharAttr
+    inc bl
+    mov al, [box_char_horiz]
+    call WriteCharAttr
+    inc bl
+    mov al, [box_char_top_r]
+    call WriteCharAttr
+
+    ; Línea media: ║ L ║ (centrada: espacio, letra, espacio)
+    inc bh
+    mov bl, cl          ; Restaurar columna base
+    mov al, [box_char_vert]
+    call WriteCharAttr
+    inc bl
+    mov al, ' '         ; Espacio antes de la letra
+    call WriteCharAttr
+    inc bl
+    mov al, dl          ; Letra o '_' (centrada)
+    call WriteCharAttr
+    inc bl
+    mov al, ' '         ; Espacio después de la letra
+    call WriteCharAttr
+    inc bl
+    mov al, [box_char_vert]
+    call WriteCharAttr
+
+    ; Línea inferior: ╚═══╝ (5 caracteres: ╚ + 3 ═ + ╝)
+    inc bh
+    mov bl, cl          ; Restaurar columna base
+    mov al, [box_char_bot_l]
+    call WriteCharAttr
+    inc bl
+    mov al, [box_char_horiz]
+    call WriteCharAttr
+    inc bl
+    mov al, [box_char_horiz]
+    call WriteCharAttr
+    inc bl
+    mov al, [box_char_horiz]
+    call WriteCharAttr
+    inc bl
+    mov al, [box_char_bot_r]
+    call WriteCharAttr
+
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    ret
+DrawBox endp
+
+DrawGuessSlots proc near
+    ; Entrada: BH = fila, BL = columna, AH = atributo
+    push ax
+    push bx
+    push cx
+    push dx
+
+    mov [render_base_column], bl
+    mov cx, 5
+    mov dh, bh          ; Guardar fila base
+    mov dl, ah          ; Guardar atributo
+
+DrawLoop:
+    mov bh, dh
+    mov al, ' '
+    mov ah, dl          ; Usar atributo pasado como parámetro
+    call DrawBox
+
+    ; Avanzar a la siguiente posición (cada recuadro ocupa 5 columnas + 1 espacio)
+    mov bl, [render_base_column]
+    add bl, 6
+    mov [render_base_column], bl
+    loop DrawLoop
+
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    ret
+DrawGuessSlots endp
+
+ReadWord proc near
+    push ax
+    push bx
+    push cx
+    push dx
+    push si
+    push di
+
+    mov [rw_base_column], dl
+    mov [rw_letter_attr], ah
+    mov [rw_base_row], dh
+    mov di, bx
+
+    mov cx, 5
+    mov si, di
+    xor al, al
+
+ClearBuffer:
+    mov [si], al
+    inc si
+    loop ClearBuffer
+
+    xor cx, cx
+
+ReadLoop:
+    xor ah, ah
+    int 16h
+    cmp al, 08h
+    je HandleBackspace
+    cmp al, 0Dh
+    je ReadLoop
+    cmp al, ' '
+    jnb ReadLoopContinue1
+    jmp InvalidKey
+ReadLoopContinue1:
+    cmp cx, 5
+    jae ReadLoop
+
+    cmp al, 'a'
+    jb CheckUpper
+    cmp al, 'z'
+    ja CheckUpper
+    sub al, 20h
+
+CheckUpper:
+    cmp al, 'A'
+    jnb CheckUpperValidLow
+    jmp InvalidKey
+CheckUpperValidLow:
+    cmp al, 'Z'
+    jbe CheckUpperDone
+    jmp InvalidKey
+CheckUpperDone:
+
+    mov si, di
+    add si, cx
+    mov [si], al
+
+    ; Calcular posición del recuadro (cada recuadro ocupa 6 columnas: 5 del recuadro + 1 espacio)
+    mov dl, al          ; Guardar letra
+    mov al, cl
+    xor ah, ah          ; Limpiar AH para multiplicación
+    mov bl, 6
+    mul bl              ; Multiplicar índice por 6 (resultado en AX)
+    mov bl, [rw_base_column]
+    add bl, al
+    mov bh, [rw_base_row]
+    mov al, dl          ; Restaurar letra
+    mov ah, [rw_letter_attr]
+    call DrawBox
+
+    inc cx
+    cmp cx, 5
+    je ReadDone
+    jmp ReadLoop
+
+HandleBackspace:
+    cmp cx, 0
+    je ReadLoop
+    dec cx
+    mov si, di
+    add si, cx
+    mov byte ptr [si], 0
+
+    ; Calcular posición del recuadro y dibujar recuadro vacío
+    mov al, cl
+    xor ah, ah          ; Limpiar AH para multiplicación
+    mov bl, 6
+    mul bl              ; Multiplicar índice por 6 (resultado en AX)
+    mov bl, [rw_base_column]
+    add bl, al
+    mov bh, [rw_base_row]
+    mov al, ' '
+    mov ah, [rw_letter_attr]
+    call DrawBox
+    jmp ReadLoop
+
+InvalidKey:
+    jmp ReadLoop
+
+ReadDone:
+    pop di
+    pop si
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    ret
+ReadWord endp
+
+EvaluateGuess proc near
+    push ax
+    push bx
+    push cx
+    push dx
+    push si
+    push di
+
+    mov bp, bx
+    mov dx, si
+
+    mov si, di
+    mov di, offset tempTarget
+    mov cx, 5
+CopyTarget:
+    mov al, [si]
+    mov [di], al
+    inc si
+    inc di
+    loop CopyTarget
+
+    mov cx, 5
+    mov di, bp
+    xor al, al
+ZeroStatuses:
+    mov [di], al
+    inc di
+    loop ZeroStatuses
+
+    mov si, dx
+    mov di, offset tempTarget
+    mov bx, bp
+    mov cx, 5
+FirstPass:
+    mov al, [si]
+    mov ah, [di]
+    cmp al, ah
+    jne FirstNext
+    mov byte ptr [bx], 2
+    mov byte ptr [di], 0
+FirstNext:
+    inc si
+    inc di
+    inc bx
+    loop FirstPass
+
+    mov si, dx
+    mov bx, bp
+    mov cx, 5
+SecondPass:
+    mov al, [si]
+    cmp byte ptr [bx], 0
+    jne SkipSecond
+    mov di, offset tempTarget
+    mov dx, 5
+SearchLoop:
+    cmp al, [di]
+    jne ContinueSearch
+    mov byte ptr [bx], 1
+    mov byte ptr [di], 0
+    jmp FoundLetter
+ContinueSearch:
+    inc di
+    dec dx
+    jnz SearchLoop
+FoundLetter:
+SkipSecond:
+    inc si
+    inc bx
+    loop SecondPass
+
+    pop di
+    pop si
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    ret
+EvaluateGuess endp
+
+RenderGuessRow proc near
+    push ax
+    push bx
+    push cx
+    push dx
+    push si
+    push di
+
+    mov [render_base_column], dl
+    mov cx, 5
+
+RenderLoop:
+    mov al, [si]
+    mov dl, [di]
+    mov ah, BASE_ATTR
+    cmp dl, 2
+    je UseGreen
+    cmp dl, 1
+    je UseYellow
+    jmp AttrReady
+
+UseGreen:
+    mov ah, GREEN_ATTR
+    jmp AttrReady
+
+UseYellow:
+    mov ah, YELLOW_ATTR
+
+AttrReady:
+    mov bl, [render_base_column]
+    mov bh, dh
+    call DrawBox
+
+    ; Avanzar a la siguiente posición (cada recuadro ocupa 5 columnas + 1 espacio)
+    mov bl, [render_base_column]
+    add bl, 6
+    mov [render_base_column], bl
+
+    inc si
+    inc di
+    loop RenderLoop
+
+    pop di
+    pop si
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    ret
+RenderGuessRow endp
+
+CalculateCenteredColumn proc near
+    ; Calcula la columna centrada para los recuadros
+    ; Cada recuadro ocupa 5 caracteres + 1 espacio = 6 caracteres
+    ; 5 recuadros × 6 = 30, pero el último no tiene espacio = 29 caracteres totales
+    ; Retorna en AL la columna inicial centrada
+    push bx
+    push cx
+    
+    ; Ancho total: 5 recuadros × 6 caracteres - 1 espacio del último = 29
+    mov al, 5           ; Número de recuadros
+    mov bl, 6           ; Ancho por recuadro (5 caracteres + 1 espacio)
+    mul bl              ; AX = 5 × 6 = 30
+    sub ax, 1           ; Restar 1 espacio del último recuadro = 29
+    
+    ; Calcular columna centrada: (80 - 29) / 2
+    mov bl, 50h         ; 80 columnas (50h)
+    sub bl, al          ; 80 - 29 = 51
+    mov al, bl
+    shr al, 1           ; Dividir por 2 = 25 (redondeado hacia abajo)
+    
+    pop cx
+    pop bx
+    ret
+CalculateCenteredColumn endp
+
+DrawBigText proc near
+    ; Dibuja el título completo desde bigW de una vez
+    ; bigW contiene el título completo con 0dh,0ah al final de cada fila
+    ; Entrada: BH = fila base, AH = atributo
+    push ax
+    push bx
+    push cx
+    push dx
+    push si
+    
+    mov dh, ah          ; Guardar atributo en DH
+    mov dl, bh          ; Guardar fila base en DL
+    lea si, bigW        ; SI apunta al inicio del título
+    
+    mov ch, 5           ; 5 filas en total
+DrawTitleRow:
+    push cx             ; Guardar contador de filas
+    mov bx, si          ; BX = inicio de fila (para restaurar después)
+    
+    ; Contar caracteres en esta fila (hasta encontrar 0dh o 0ah)
+    xor cx, cx          ; Contador de caracteres (limpiar CX)
+CountRowChars:
+    lodsb               ; Leer carácter
+    cmp al, 0Dh         ; ¿Es retorno de carro?
+    je RowLengthFound
+    cmp al, 0Ah         ; ¿Es nueva línea?
+    je RowLengthFound
+    inc cx
+    jmp CountRowChars
+    
+RowLengthFound:
+    mov si, bx          ; Restaurar SI al inicio de la fila
+    
+    ; Calcular columna centrada para esta fila
+    mov ax, 80          ; Ancho de pantalla
+    sub ax, cx          ; 80 - longitud de fila
+    shr ax, 1           ; Dividir por 2
+    mov bl, al          ; BL = columna inicial centrada
+    mov bh, dl          ; BH = fila actual
+    
+    ; Dibujar la fila completa
+    mov ah, dh          ; Restaurar atributo
+DrawRowChar:
+    lodsb               ; Leer carácter
+    cmp al, 0Dh         ; ¿Es retorno de carro?
+    je SkipNewline
+    cmp al, 0Ah         ; ¿Es nueva línea?
+    je RowDone
+    call WriteCharAttr
+    inc bl
+    jmp DrawRowChar
+    
+SkipNewline:
+    lodsb               ; Saltar el 0Ah también (SI ahora apunta a la siguiente fila)
+RowDone:
+    inc dl              ; Siguiente fila
+    pop cx              ; Restaurar contador de filas
+    ; SI ya apunta al inicio de la siguiente fila (después del 0dh,0ah)
+    dec ch
+    jnz DrawTitleRow
+    
+    pop si
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    ret
+DrawBigText endp
+
+DrawBigLetter proc near
+    ; Dibuja una letra grande de 5x11
+    ; Entrada: SI = puntero al patrón de la letra (55 bytes: 5 filas × 11 columnas)
+    ;          BL = columna base, BH = fila base, AH = atributo
+    push ax
+    push bx
+    push cx
+    push dx
+    push si
+    
+    mov dl, bl          ; Guardar columna base en DL
+    mov dh, bh          ; Guardar fila base en DH
+    mov [big_letter_attr], ah  ; Guardar atributo en variable
+    
+    mov ch, 5           ; Contador de filas (5 filas)
+DrawLetterRow:
+    push cx             ; Guardar contador de filas
+    mov bl, dl          ; Restaurar columna base
+    mov bh, dh          ; Restaurar fila base
+    mov cl, 11          ; Contador de columnas (11 columnas)
+DrawLetterCol:
+    lodsb               ; Cargar carácter del patrón
+    mov ah, [big_letter_attr]  ; Restaurar atributo desde variable
+    call WriteCharAttr
+    inc bl
+    dec cl
+    jnz DrawLetterCol
+    inc dh              ; Siguiente fila
+    pop cx              ; Restaurar contador de filas
+    dec ch
+    jnz DrawLetterRow
+    
+    pop si
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    ret
+DrawBigLetter endp
+
+end
+
+
